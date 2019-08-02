f4122174ed6da0c3436a61a9da4c7c49c9f59550
Darin Webster <darinleenavy@gmail.com>
2019-08-02 17:06:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog 
+  def name=(the_dog_name)
+    @the_dog = the_dog_name
+  end
   
+  def name
+    @name
+  end
+  
+  def breed=(the_dog_breed)
+    @the_breed = the_dog_breed
+  end
+  
+  def breed
+    @breed
+  end
 end
